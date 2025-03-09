ab23b272a58a60438dc73f3d730d6a9e4514ae61
md-shakib-khan <mdshakibkhan.dev@gmail.com>
2025-03-09 14:26:13 +0600

    1

---
+++ prisma/migrations/20250309082558_changed_api_keys/migration.sql
@@ -0,0 +1,60 @@
+/*
+  Warnings:
+
+  - The `keyType` column on the `ApiKey` table would be dropped and recreated. This will lead to data loss if there is data in the column.
+
+*/
+-- CreateEnum
+CREATE TYPE "API_KEY_TYPE" AS ENUM ('BRONZE', 'SILVER', 'GOLD');
+
+-- DropForeignKey
+ALTER TABLE "ApiKey" DROP CONSTRAINT "ApiKey_ownerId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Book" DROP CONSTRAINT "Book_authorId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Favorite" DROP CONSTRAINT "Favorite_bookId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Favorite" DROP CONSTRAINT "Favorite_userId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Rating" DROP CONSTRAINT "Rating_bookId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Rating" DROP CONSTRAINT "Rating_userId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Review" DROP CONSTRAINT "Review_bookId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Review" DROP CONSTRAINT "Review_userId_fkey";
+
+-- AlterTable
+ALTER TABLE "ApiKey" DROP COLUMN "keyType",
+ADD COLUMN     "keyType" "API_KEY_TYPE" NOT NULL DEFAULT 'BRONZE';
+
+-- AddForeignKey
+ALTER TABLE "Book" ADD CONSTRAINT "Book_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Review" ADD CONSTRAINT "Review_bookId_fkey" FOREIGN KEY ("bookId") REFERENCES "Book"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Review" ADD CONSTRAINT "Review_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Rating" ADD CONSTRAINT "Rating_bookId_fkey" FOREIGN KEY ("bookId") REFERENCES "Book"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Rating" ADD CONSTRAINT "Rating_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Favorite" ADD CONSTRAINT "Favorite_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Favorite" ADD CONSTRAINT "Favorite_bookId_fkey" FOREIGN KEY ("bookId") REFERENCES "Book"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ApiKey" ADD CONSTRAINT "ApiKey_ownerId_fkey" FOREIGN KEY ("ownerId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
